89ea9478d50f7004a4bab36904e49d08ef28631f
Henrik Habbestad Andersen <henrikhabbe92@gmail.com>
2022-11-30 09:52:34 +0100

    Started project, fetch api with axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [pokemons, setPokemons] = useState("");
+  const [pokemon, setPokemon] = useState("");
+
+  useEffect(() => {
+    const url = "https://pokeapi.co/api/v2/pokemon?offset=40&limit=40";
+
+    axios.get(url).then((response) => {
+      setPokemons(response.data.results);
+    });
+  }, []);
+
+  useEffect(() => {
+    const url = "https://pokeapi.co/api/v2/pokemon/1";
+
+    axios.get(url).then((response) => {
+      setPokemon(response.data);
+      console.log(response.data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>POKEDEX</h1>
+      </div>
+      <div>
+        {pokemons
+          ? pokemons.map((pokemon) => {
+              return <p>{pokemon.name}</p>;
+            })
+          : null}
+      </div>
+    </>
   );
 }
 
